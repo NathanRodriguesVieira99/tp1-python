49e14fcb1983b32838533283055864ca9880b27a
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-04 17:02:34 -0300

    feat: exercices 1 & 2 done

--- ex1.py
+++ ex1.py
@@ -1,2 +1,8 @@
-print('teste')
-print('teste push colab')
+nome_estagiario = 'Nathan Vieira'
+idade_estagiario = 19
+codigo_departamento = 'TI001'
+
+print('--- CADASTRO DE ESTAGIÁRIO ---')
+print(f"Nome: {nome_estagiario}")
+print(f"Idade: {idade_estagiario}")
+print(f"Código de departamento: {codigo_departamento}")
--- ex2.py
+++ ex2.py
@@ -1,1 +1,9 @@
-print('ex2')
+nome_cliente = 'Nathan Vieira'
+idade_cliente = 19
+score_credito = 772.4
+cliente_ativo = True
+
+print(f"nome_cliente: {nome_cliente} - Tipo: {type(nome_cliente)}")
+print(f"idade_cliente: {idade_cliente} - Tipo: {type(idade_cliente)}")
+print(f"score_credito: {score_credito} - Tipo: {type(score_credito)}")
+print(f"cliente_ativo: {cliente_ativo} - Tipo: {type(cliente_ativo)}")
