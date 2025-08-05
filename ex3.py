e5e59a4947c95671a6f7fcda253e26e4f5a763e8
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-08-05 16:44:21 -0300

    feat: ex3 done

--- ex3.py
+++ ex3.py
@@ -1,1 +1,25 @@
-print('ex3')
+km_por_dia = 19
+ANO_2_digitos = 7
+gasto_diario = 300 + ANO_2_digitos
+
+print("--- CÁLCULOS  ---")
+print(f"Quilômetros por dia: {km_por_dia} km")
+print(f"Gasto diário: R$ {gasto_diario}")
+print()
+
+print("--- RESULTADOS ---")
+
+total_semana = km_por_dia * 7
+print(f"1. Total em uma semana: {total_semana} km")
+
+diferenca = 100 - gasto_diario
+print(f"2. Diferença entre R$100 e gasto diário: R$ {diferenca}")
+
+dias_cobertura = 500 // gasto_diario
+print(f"3. Quantos dias R$500 cobre: {dias_cobertura} dias")
+
+resto = gasto_diario % 100
+print(f"4. Resto da divisão do gasto diário por 100: {resto}")
+
+custo_por_km = gasto_diario / km_por_dia
+print(f"5. Custo por km: R$ {custo_por_km:.2f}")
